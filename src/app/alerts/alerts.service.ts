eac3768ea4a223900341900fad066838f7459ab0
cellmaniadesarrollo <christians9210023.tcm@gmail.com>
2025-07-31 10:01:03 -0500

    cambios de toast

--- src/app/alerts/alerts.service.ts
+++ src/app/alerts/alerts.service.ts
@@ -10,29 +10,29 @@ export class AlertsService {
   constructor(private toast: ToastrService,private toast2: NgToastService) {}
 
   showSuccess(texto:any, titulo:any) {
-   //console.log(texto)
-   Swal.fire({ 
-    position: "top-end",
-    icon: "success",
-    title: titulo,
-    text: texto,
-    showConfirmButton: false,
-    timer: 2500
-  });
-   //this.toast2.success({detail:titulo,summary:texto,duration:2000});
-   // this.toast.success(texto, titulo);
+    console.log('titulo',titulo)
+  //  Swal.fire({ 
+  //   position: "top-end",
+  //   icon: "success",
+  //   title: titulo,
+  //   text: texto,
+  //   showConfirmButton: false,
+  //   timer: 2500
+  // });
+   // this.toast2.success({detail:titulo,summary:texto,duration:2000});
+     this.toast.success(texto,  (typeof titulo === 'object') ? '' : titulo);
   }
   showError(texto:any, titulo:any) {
-    Swal.fire({
-      position: "top-end",
-      icon: "error",
-      title: titulo,
-      text: texto,
-      showConfirmButton: false,
-      timer: 2500,
-      width: '300px'
-    });
-  //  this.toast2.error({detail:titulo,summary:texto,duration:2000});
-   // this.toast.error(texto, titulo);
+    // Swal.fire({
+    //   position: "top-end",
+    //   icon: "error",
+    //   title: titulo,
+    //   text: texto,
+    //   showConfirmButton: false,
+    //   timer: 2500,
+    //   width: '300px'
+    // });
+  // this.toast2.error({detail:titulo,summary:texto,duration:2000});
+     this.toast.error(texto,   (typeof titulo === 'object') ? '' : titulo);
   }
 }
